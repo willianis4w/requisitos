4e1bec3240f7d4b402ee55200a6fcdfbce487587
Adan Felipe Medeiros <adan.grg@gmail.com>
2013-03-19 00:32:05 -0300

    Removido silex

--- web/gzip-js.php
+++
@@ -1,6 +0,0 @@
-<?  ob_start('ob_gzhandler');  
-  header ("content-type: text/javascript; charset: UTF-8");  
-  header ("cache-control: must-revalidate");  
-  $offset = 60 * 60;  
-  $expire = "expires: " . gmdate ("D, d M Y H:i:s", time() + $offset) . " GMT";  
-  header ($expire); ?>  
